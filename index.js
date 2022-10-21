0f312165887eb1c902f7b64148b5a217a294adee
untunt <unt_tianjin@163.com>
2022-10-21 23:30:17 +0800

    Add dictionary and deriver

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
 let countTotal = null;
 let rimeCounts = [];
 let pairCounts = [];
-let rimes = '';
-let rimesMini = '';
+let material = [];
+let rimeNames = [];
+let rimeNamesMini = '';
+const inputIds = ['material', 'delimiters', 'dictionary', 'deriver-code', 'rime-names', 'rime-names-mini'];
+
+function getInput(i) { return document.getElementById(inputIds[i]).value; }
+function setInput(i, str) { document.getElementById(inputIds[i]).value = str; }
 
 // 讀入所有韻部總字次
 function loadCountTotal() {
   countTotal = document.getElementById('countTotal').value;
 }
 
-// 從文本框中讀入資料
+// 從文本框中讀入資料，標注好各韻腳的韻部名，計算字次韻次
 function loadData() {
   rimeCounts = [];
   pairCounts = [];
-  rimes = '';
-  rimesMini = '';
-  let material = document.getElementById('material');
-  let rimeNames = document.getElementById('rimeNames');
-  let delimiters = document.getElementById('delimiters');
-  let rimeNamesMini = document.getElementById('rimeNamesMini');
-  if (material.value) {
-    let materialValue = material.value.replace(RegExp(`[${delimiters.value}]`, 'g'), '\n');
-    materialValue.split('\n').forEach(line => {
-      line = splitRimeNames(line);
-      let rime1 = line[0];
-      line.slice(1).forEach(rime2 => {
-        let pair = getPair(rime1, rime2);
-        rimeCounts[rime1] = (rimeCounts[rime1] || 0) + 1;
-        rimeCounts[rime2] = (rimeCounts[rime2] || 0) + 1;
-        pairCounts[pair] = (pairCounts[pair] || 0) + 1;
-        rime1 = rime2;
-      });
-    });
-    // 留空時，使用 rimeCounts 的全部 keys
-    rimes = rimeNames.value ? splitRimeNames(rimeNames.value) : Object.keys(rimeCounts);
-    rimesMini = rimeNamesMini.value;
+  if (!getInput(0)) return;
+  material = getInput(0);
+  let delimiters = getInput(1);
+  let dictionary = getInput(2);
+  let deriverCode = getInput(3);
+  rimeNames = getInput(4);
+  rimeNamesMini = getInput(5);
+
+  // 拆分字頭和標注
+  function splitAnnotation(text, fillAndDeriveAnnotation = false) {
+    if (!text) return [];
+    text = text.replace(/[ \t,，\(\)（）]/g, '');
+    text = [...text];
+    text = [text[0], text.slice(1).join('')];
+    if (fillAndDeriveAnnotation) {
+      text[1] = text[1] || dictionary[text[0]];
+      let 音韻地位;
+      try { 音韻地位 = Qieyun.音韻地位.from描述(text[1]); } catch (_) {
+        try { 音韻地位 = Qieyun.音韻地位.from編碼(text[1]); } catch (_) { }
+      }
+      // 若標注無法轉爲音韻地位，則默認其已經是韻部名，跳過推導
+      if (音韻地位) text[1] = eval('(_ => {' + (deriverCode || 'return 音韻地位.韻;') + '})()');
+    };
+    return text;
   }
+  dictionary = Object.fromEntries(dictionary.split('\n').map(splitAnnotation));
+
+  material = material.replace(RegExp(`[${delimiters}]`, 'g'), '\n').trim();
+  material = material.split('\n').map(line => {
+    line = line.replace(/(.([\(（].*?[\)）])?)/g, '$1 ');
+    line = line.replace(/ +/g, ' ');
+    line = line.trim();
+    return line.split(' ').map(e => splitAnnotation(e, true));
+  });
+  material.forEach(line => {
+    let e1 = line[0];
+    line.slice(1).forEach(e2 => {
+      let pair = getPair(e1[1], e2[1]);
+      rimeCounts[e1[1]] = (rimeCounts[e1[1]] || 0) + 1;
+      rimeCounts[e2[1]] = (rimeCounts[e2[1]] || 0) + 1;
+      pairCounts[pair] = (pairCounts[pair] || 0) + 1;
+      e1 = e2;
+    });
+  });
+  // 留空時，使用 rimeCounts 的全部 keys
+  rimeNames = rimeNames ? rimeNames.split(' ') : Object.keys(rimeCounts);
 }
 
-// 拆分（解析）韻類名
-function splitRimeNames(rimeNames) {
-  // 支持的格式：
-  //  腳(韻)腳(韻)腳(韻)
-  //  韻,韻,韻
-  //  韻\t韻\t韻
-  //  韻 韻 韻
-  //  韻韻韻
-  rimeNames = [...rimeNames].map((e, i, arr) => i < arr.length - 1 && arr[i + 1] === '(' ? '' : e).join('');
-  rimeNames = rimeNames.replace(/[\(\),\t]/g, ' ');
-  rimeNames = rimeNames.replace(/ +/g, ' ');
-  rimeNames = rimeNames.trim();
-  return rimeNames.includes(' ') ? rimeNames.split(' ') : [...rimeNames];
+// 韻譜轉爲文字版（需自行在瀏覽器控制台調用）
+function materialToText() {
+  return material.map(line => line.map(e => e[0] + '(' + e[1] + ')').join('')).join('\n');
+}
+
+// 獲取無標注的韻腳字
+function getUnannotated() {
+  let unannotated = [];
+  material.forEach(line => line.forEach(e => {
+    if (!e[1] && !unannotated.includes(e[0])) unannotated.push(e[0]);
+  }));
+  return unannotated;
 }
 
 // 獲取排好序的兩韻
@@ -78,7 +105,7 @@ function getCellValue(content) {
 }
 
 // 獲取內容是韻類名的單元格
-function getCellRimeName(content, rimeNamesMini = '', isTh = false) {
+function getCellRimeName(content, isTh = false) {
   if (!content) return getCell();
   content = [...content].map(e => rimeNamesMini.includes(e) ? `(${e})` : e).join('');
   content = content.replace(/\)\(/g, '');
@@ -119,13 +146,16 @@ function genTable(checks) {
   // 重設表格
   let table = document.getElementById('table');
   let message = '';
-  if (!rimes) {
+  if (!rimeNames.length) {
     message = '請正確輸入韻段！';
   } else if (checks.rhymeGroup && countTotal < 2) {
     message = '請正確輸入總字次！';
+  } else {
+    let unannotated = getUnannotated();
+    if (unannotated.length) message = '以下字無標注！</div><div style="margin-top: 0.2em;">' + unannotated.join('<br>');
   }
   if (message) {
-    table.innerHTML = '<tr style="color: red;"><th><span>' + message + '</span></th></tr>';
+    table.innerHTML = '<tr><th style="line-height: 1.3;"><div style="color: red;">' + message + '</div></th></tr>';
     return;
   }
   table.innerHTML = '';
@@ -136,13 +166,13 @@ function genTable(checks) {
   headRow.appendChild(getCell('字次', true));
   table.appendChild(headRow);
 
-  rimes.forEach((rime1, i) => {
-    headRow.appendChild(getCellRimeName(rime1, rimesMini, true));
+  rimeNames.forEach((rime1, i) => {
+    headRow.appendChild(getCellRimeName(rime1, true));
     let count1 = rimeCounts[rime1] || 0;
     let row = document.createElement('tr');
-    row.appendChild(getCellRimeName(rime1, rimesMini));
+    row.appendChild(getCellRimeName(rime1));
     row.appendChild(getCellValue(count1));
-    rimes.forEach((rime2, j) => {
+    rimeNames.forEach((rime2, j) => {
       let count2 = rimeCounts[rime2] || 0;
       let count11 = pairCounts[getPair(rime1, rime1)] || 0;
       let count12 = pairCounts[getPair(rime1, rime2)] || 0;
@@ -249,33 +279,32 @@ function arrangeOutput() {
 }
 
 // 加載示例資料
-function writeSampleData(rimeNamesMiniOnly = false) {
-  const materialSample = '衡(庚二)横(庚二)亨(庚二)行(庚二)\n客(庚二)陌(庚二)\n泽(庚二)客(庚二)\n鳠(耕)頟(庚二)白(庚二)宅(庚二)\n形(青)灵(青)冥(青)龄(青)\n声(清)英(庚三)\n诚(清)明(庚三)牲(庚莊)缨(清)盈(清)声(清)并(清)\n声(清)精(清)英(庚三)\n英(庚三)清(清)平(庚三)\n城(清)鸣(庚三)\n盈(清)平(庚三)声(清)生(庚莊)成(清)\n岭(清)秉(庚三)景(庚三)永(庚三)聘(清)请(清)\n圣(清)命(庚三)\n碧(清)石(清)\n辟(清)籍(清)役(清)\n积(清)石(清)脊(清)\n适(清)尺(清)迹(清)石(清)\n嵉(青)星(青)平(庚三)形(青)经(青)成(清)垧(青)萦(清)青(青)\n明(庚三)成(清)灵(青)盈(清)声(清)情(清)\n茎(耕)莺(耕)惊(庚三)\n摘(耕)襞(清)射(清)隙(庚三)席(清)役(清)惜(清)';
-  const rimeNamesSample = '青 清 庚莊 庚三 耕 庚二';
-  const delimitersSample = '□；‖';
-  const rimeNamesMiniSample = '一二三四五六七八九十等 1234567890 ABCD 開开合洪細细撮口呼 內内外輕重紐纽鈕钮類类 幫帮非端知來来精莊庄章日照見见影喻云以 脣唇齒齿舌牙喉 銳鋭锐鈍钝 平上去入仄陰';
+function writeSampleData(rimeNamesMiniOnly = false, noDictionary = false) {
+  const sample = [
+    noDictionary ?
+      '衡(庚二)横(庚二)亨(庚二)行(庚二)\n客(庚二)陌(庚二)\n泽(庚二)客(庚二)\n鳠(耕)頟(庚二)白(庚二)宅(庚二)\n形(青)灵(青)冥(青)龄(青)\n声(清)英(庚三)\n诚(清)明(庚三)牲(庚莊)缨(清)盈(清)声(清)并(清)\n声(清)精(清)英(庚三)\n英(庚三)清(清)平(庚三)\n城(清)鸣(庚三)\n盈(清)平(庚三)声(清)生(庚莊)成(清)\n岭(清)秉(庚三)景(庚三)永(庚三)聘(清)请(清)\n圣(清)命(庚三)\n碧(庚三)石(清)\n辟(清)籍(清)役(清)\n积(清)石(清)脊(清)\n适(清)尺(清)迹(清)石(清)\n嵉(青)星(青)平(庚三)形(青)经(青)成(清)垧(青)萦(清)青(青)\n明(庚三)成(清)灵(青)盈(清)声(清)情(清)\n茎(耕)莺(耕)惊(庚三)\n摘(耕)襞(清)射(清)隙(庚三)席(清)役(清)惜(清)' :
+      '衡横亨行(匣開二庚平)\n客陌\n泽客\n鳠頟白宅\n形灵冥龄\n声英\n诚明牲缨盈声并\n声精英\n英清平\n城鸣\n盈平声生成\n岭秉景永聘请\n圣命\n碧石\n辟籍役\n积石脊\n适尺迹石\n嵉星平形经成垧萦青\n明成灵盈声情\n茎莺惊\n摘襞射隙席役惜',
+    '□；。‖',
+    noDictionary ? '' : '衡(匣開二庚平)\n横(匣合二庚平)\n亨(曉開二庚平)\n客(溪開二庚入)\n陌(明二庚入)\n泽(澄開二庚入)\n鳠(匣合二耕入)\n頟(疑開二庚入)\n白(並二庚入)\n宅(澄開二庚入)\n形(匣開四青平)\n灵(來開四青平)\n冥(明四青平)\n龄(來開四青平)\n声(書開三清平)\n英(影開三庚平)\n诚(常開三清平)\n明(明三庚平)\n牲(生開三庚平)\n缨(影開三清平)\n盈(以開三清平)\n并(幫三清平)\n精(精開三清平)\n清(清開三清平)\n平(並三庚平)\n城(常開三清平)\n鸣(明三庚平)\n生(生開三庚平)\n成(常開三清平)\n岭(來開三清上)\n秉(幫三庚上)\n景(見開三庚上)\n永(云合三庚上)\n聘(滂三清去)\n请(清開三清上)\n圣(書開三清去)\n命(明三庚去)\n碧(幫三庚入)\n石(常開三清入)\n辟(幫三清入)\n籍(從開三清入)\n役(以合三清入)\n积(精開三清入)\n脊(精開三清入)\n适(書開三清入)\n尺(昌開三清入)\n迹(精開三清入)\n嵉(定開四青平)\n星(心開四青平)\n经(見開四青平)\n垧(見合四青平)\n萦(影合三清平)\n青(清開四青平)\n情(從開三清平)\n茎(匣開二耕平)\n莺(影開二耕平)\n惊(見開三庚平)\n摘(知開二耕入)\n襞(幫三清入)\n射(船開三清入)\n隙(溪開三庚入)\n席(邪開三清入)\n惜(心開三清入)',
+    noDictionary ? '' : "return 音韻地位.判斷([\n  ['庚韻 莊組', '庚莊'],\n  ['庚韻', '庚' + 音韻地位.等],\n  ['', 音韻地位.韻], // 其餘韻母\n]);",
+    '青 清 庚莊 庚三 耕 庚二',
+    '一二三四五六七八九十等 1234567890 ABCD 開开合洪細细撮口呼 內内外輕重紐纽鈕钮類类 幫帮非端知來来精莊庄章日照見见影喻云以 脣唇齒齿舌牙喉 銳鋭锐鈍钝 平上去入仄陰',
+  ];
   const countTotalSample = 1000;
-  let material = document.getElementById('material');
-  let rimeNames = document.getElementById('rimeNames');
-  let delimiters = document.getElementById('delimiters');
-  let rimeNamesMini = document.getElementById('rimeNamesMini');
-  let countTotal = document.getElementById('countTotal');
 
   if (rimeNamesMiniOnly) {
-    delimiters.value = delimitersSample;
-    rimeNamesMini.value = rimeNamesMiniSample;
+    setInput(1, sample[1]); // delimiters
+    setInput(5, sample[5]); // rimeNamesMini
     return false;
   }
-  if (material.value
-    || rimeNames.value
-    || rimeNamesMini.value && rimeNamesMini.value !== rimeNamesMiniSample) {
+  if (getInput(0) || getInput(2) || getInput(3) || getInput(4)
+    || getInput(1) && getInput(1) !== sample[1]
+    || getInput(5) && getInput(5) !== sample[5]
+  ) {
     if (!confirm('示例資料會覆蓋現有資料，確認加載？')) return false;
   }
-  material.value = materialSample;
-  rimeNames.value = rimeNamesSample;
-  delimiters.value = delimitersSample;
-  rimeNamesMini.value = rimeNamesMiniSample;
-  countTotal.value = countTotalSample;
+  sample.forEach((e, i) => { setInput(i, e); });
+  document.getElementById('countTotal').value = countTotalSample;
   return true;
 }
 
